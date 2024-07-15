908bbbe188977dd7d63f3dc138b5d040d2c2a7de
Belynn <deborahclerckx@gmail.com>
2024-07-15 19:55:56 +0200

    🩹 Update config.php

--- assets/db/config.php
+++ assets/db/config.php
@@ -1,5 +1,5 @@
 <?php
-    const DSN = "mysql:host=localhost;dbname=insectopedia;port=3306;charset=utf8";
+    const DSN = "mysql:host=localhost;dbname=fp_insectopedia;port=3306;charset=utf8";
     const USERNAME = "root";
     const PASSWORD = "";
 ?>
