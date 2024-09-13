49a4f1bdb27f139046da78aacfb9687a1b0f78da
Deborah Clerckx <d.clerckx@interface3.be>
2024-09-13 12:13:39 +0200

    test en cours EOD

--- migrations/Version20240913064307.php
+++ migrations/Version20240913064307.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240912170952 extends AbstractMigration
+final class Version20240913064307 extends AbstractMigration
 {
     public function getDescription(): string
     {
