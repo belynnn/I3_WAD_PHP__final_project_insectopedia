2c5d1afac7a60173a655a75b149f88ee9b74f101
Deborah Clerckx <d.clerckx@interface3.be>
2024-10-01 16:25:07 +0200

    test install webpack npm bootstrap

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,15 @@
-import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
  */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/app.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+const $ = require ('jquery');
+window.jQuery = $;
+window.$ = $;
