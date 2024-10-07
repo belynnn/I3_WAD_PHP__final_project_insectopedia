624b663175885bfd1a6c59535efdab6bdebd6202
Deborah Clerckx <d.clerckx@interface3.be>
2024-10-07 15:08:40 +0200

    lil' refacto insectcontroller, add route from form add insect (because ça me perturbe d'avoir 4785 controller lul)

--- src/Controller/InsectController.php
+++ src/Controller/InsectController.php
@@ -3,13 +3,17 @@
 namespace App\Controller;
 
 use App\Entity\Insect;
+use App\Form\InsectType;
 use App\Repository\InsectRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
 class InsectController extends AbstractController
 {
@@ -60,6 +64,60 @@ class InsectController extends AbstractController
             'insect' => $insect,
             'isFavorite' => $isFavorite,
         ]);
+    }
+
+    ///////////////////////////////////////////////////////////////////////
+    //
+    // Page de formulaire pour ajouter un insecte, disponible uniquement par les administrateurices
+    //
+    #[Route('/insectes/ajouter', name: 'app_formAddInsect')]
+    public function addInsect(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+    {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            // Redirige vers la page d'accueil si l'utilisateur n'a pas le rôle ADMIN
+            return $this->redirectToRoute('app_accueil');
+        }
+
+        $insect = new Insect();
+
+        $form = $this->createForm(InsectType::class, $insect);
+
+        // gérer l'objet Request, contiendra GET ou POST
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            //
+            // Gestion de l'image
+            //
+            $imageFile = $form->get('image')->getData();
+            
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // Gérer l'exception si nécessaire
+                }
+
+                // Stocker le nom du fichier dans l'entité Observation
+                $insect->setImage($newFilename);
+            }
+
+            $entityManager->persist($insect);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_insects');
+        }
+
+        $vars = ['formInsect' => $form];
+
+        return $this->render('insect/addInsect.html.twig', $vars);
     }
 
     ///////////////////////////////////////////////////////////////////////
