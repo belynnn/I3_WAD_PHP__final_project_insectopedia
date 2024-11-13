28a942eefaf8ae79c44e053fffc945712a481d90
Deborah Clerckx <d.clerckx@interface3.be>
2024-11-13 10:20:25 +0100

    restruct before prod + test + last push prod

---
+++ migration.sh
@@ -0,0 +1,6 @@
+rm migrations/Ver*
+symfony console doctrine:database:drop --force --no-interaction
+symfony console doctrine:database:create
+symfony console make:migration --no-interaction
+symfony console doctrine:migrations:migrate --no-interaction
+symfony console doctrine:fixtures:load --no-interaction
